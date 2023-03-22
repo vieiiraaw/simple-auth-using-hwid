e008b53fb0710b25032483c26722d1b6178fa63b
fael <4afa3lbr@gmail.com>
2023-03-22 02:28:41 -0300

    up

--- routers/init.js
+++ routers/init.js
@@ -17,7 +17,8 @@ async function init(req, res) {
     return res
       .status(200)
       .json({ message: 'Initialized', key: findApplication.key });
-  } catch {
+  } catch (err) {
+    console.log(err);
     return res.status(500).json({ message: 'invalid error' });
   }
 }
--- routers/login.js
+++ routers/login.js
@@ -4,6 +4,9 @@ async function login(req, res) {
   try {
     const { userId, hwid } = req.query;
 
+    const now = new Date();
+    user.deleteMany({ expiry: { $lt: now } });
+
     const userAgent = req.headers['user-agent'];
     if (userAgent != 'know')
       return res.status(401).json({ message: 'Unauthorized' });
@@ -16,9 +19,6 @@ async function login(req, res) {
     if (!findUser)
       return res.status(404).json({ message: 'Discord ID not found' });
 
-    const now = new Date();
-    findUser.deleteMany({ expiry: { $lt: now } });
-
     if (findUser?.hwid != null && findUser?.hwid != hwid)
       return res.status(401).json({ message: 'Unauthorized' });
 
@@ -30,7 +30,8 @@ async function login(req, res) {
       username: findUser.username,
       expiry: findUser.expiry,
     });
-  } catch {
+  } catch (err) {
+    console.log(err);
     return res.status(500).json({ message: 'invalid error' });
   }
 }
